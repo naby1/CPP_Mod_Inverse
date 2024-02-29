e7affdbb5aa8be63b0b2212e6c3d8fec443d17f7
naby1 <149854487+naby1@users.noreply.github.com>
2024-02-29 21:41:17 +0800

    Add files via upload

---
+++ RSA_findD.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <openssl/bn.h>
+using namespace std;
+
+int main() {
+
+    BIGNUM* e = BN_new();
+    const char* e1 = "65537";
+    BN_dec2bn(&e, e1);
+
+    BIGNUM* p = BN_new();
+    const char* p1 = "310156213009687457743447984426171582459";
+    BN_dec2bn(&p, p1);
+
+    BIGNUM* q = BN_new();
+    const char* q1 = "176665284026927798395694329971348987271";
+    BN_dec2bn(&q, q1);
+
+    BN_CTX* ctx = BN_CTX_new();
+
+    BIGNUM* n = BN_new();
+    BN_mul(n, p, q, ctx);
+
+    BIGNUM* num1 = BN_new();
+    const char* strnum1 = "1";
+    BN_dec2bn(&num1, strnum1);
+    BIGNUM* p_1 = BN_new();
+    BN_sub(p_1, p, num1);
+    BIGNUM* q_1 = BN_new();
+    BN_sub(q_1, q, num1);
+
+    BIGNUM* fain = BN_new();
+    BN_mul(fain, p_1, q_1, ctx);
+
+    BIGNUM* d = BN_new();
+    BN_mod_inverse(d, e, fain, ctx);
+    char* d1 = BN_bn2dec(d);
+    cout << "dec_d:" << d1 << endl;
+
+    return 0;
+}
